1b176831e3b0fc8e18f733bc2dcd3d7932b21121
Patryk Stronski <p.stronski13@gmail.com>
2020-09-13 06:32:41 +0200

    sofar done

---
+++ db_import/__init__.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append('/home/azath/Documents/THESIS/DisambiguationApp/db_import/knowledge_bases')
+import dbpedia_import
+
+dbpedia_import.fetch()
--- db_import/index.py
+++
@@ -1,3 +0,0 @@
-import knowledge_base
-import config
-
