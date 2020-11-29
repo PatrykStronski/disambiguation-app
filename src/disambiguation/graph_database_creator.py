38c274f5c0a9e3c8805a3eb465e41d47e480020d
Patryk Stronski <p.stronski13@gmail.com>
2020-11-29 02:32:22 +0100

    removed prints

--- src/disambiguation/graph_database_creator.py
+++ src/disambiguation/graph_database_creator.py
@@ -38,7 +38,6 @@ class GraphDatabaseCreator:
         sum_depth_distribution = [0] * 20
         for node_index in range(start, end):
             (node_uri, node_properties) = self.neo4j_mgr.get_node_by_index(node_index);
-            print(node_uri)
             is_processed = self.redis_checker.get_processed_id(node_uri)
             if is_processed == "DONE":
                 print("The node has already been processed. Carrying on...")
@@ -46,12 +45,8 @@ class GraphDatabaseCreator:
             init_graph = InitialGraph(node_uri, node_properties, self.max_depth, self.threshold_visits, self.restart_probability, self.neo4j_mgr, self.neo4j_new)
             ret = init_graph.random_walk_with_restart()
             self.redis_checker.insert_processed_id(node_uri)
-            sum_depth_distribution = self.add_depth_distributions(sum_depth_distribution, ret[1])
-            print("In SemSign " + str(ret[0]) + " Nodes")
-            sum_entries += ret[0]
-            #init_graph.get_graph()
             if node_index % 100 == 0:
-                print("Graph creted for node " + str(node_index))
+                print("Created semsign for: " + str(node_index))
                 if node_index % 2000 == 0:
                     del self.neo4j_mgr
                     del self.neo4j_new
@@ -61,5 +56,3 @@ class GraphDatabaseCreator:
                     self.neo4j_new = Neo4jDb(self.db_dest_uri, self.db_dest)
                     self.redis_checker = RedisChecker()
             init_graph.insert_graph()
-        print("Avg number of nodes: " + str(sum_entries/(end-start)))
-        self.present_depth_distribution(sum_depth_distribution, end-start)
