9e7de605f5dbdf361f0d243ef6496463c63819ee
p.stronski13@gmail.com <p.stronski13@gmail.com>
2020-11-30 02:31:57 +0100

    changes on machine

--- docker/python/entrypoint.sh
+++ docker/python/entrypoint.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
 dockerize -wait tcp://neo_src:7687 -wait tcp://neo_dest:7687
-if [ ${APP_MODE} = "graph-init" ]
-then
-  /app/pm2_setup.sh ${PM2_PROC_NUM}
-  while true; do
-    sleep 60
-  done
-elif [ ${APP_MODE} = "test" ]
-then
-  /app/start_test.sh
-else
-  cd /app/src
-  python __init__.py
-fi
+/app/pm2_setup.sh ${PM2_PROC_NUM}
+while true; do
+  sleep 60
+done
