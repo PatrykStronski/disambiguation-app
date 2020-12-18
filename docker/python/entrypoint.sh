d1690d78a02dc5e776ec3f76ab570315ce664cab
p.stronski13@gmail.com <p.stronski13@gmail.com>
2020-12-18 01:45:29 +0100

    to test

--- docker/python/entrypoint.sh
+++ docker/python/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+conda env create -f environment.yml
+conda activate disambiguation_graph
+python -m spacy download en_core_web_sm
 dockerize -wait tcp://neo_src:7687 -wait tcp://neo_dest:7687
 /app/pm2_setup.sh ${PM2_PROC_NUM}
 while true; do
