75c484196feab74d59a2ae6388a316e874dee038
Patryk Stronski <p.stronski13@gmail.com>
2020-12-25 01:36:28 +0100

    working on CPU

--- src/services/neo4j_db.py
+++ src/services/neo4j_db.py
@@ -32,7 +32,11 @@ class Neo4jDb:
                     (c) -[r_triangle]-> (n) WHERE c.princeton = '+ princeton +' \
                     RETURN b.uri AS node2, r AS relation, r2 AS relation2, COUNT(r_triangle) AS weight'
             elif princeton:
-                print("TO IMPLEMENT")
+                query = 'MATCH (n:Resource {uri: "' + node + '"}) -[r]-> (b:Resource) WHERE ANY (x IN b.skos__prefLabel WHERE x CONTAINS "@en") \
+                    OPTIONAL MATCH (b) -[r2]-> (n) \
+                    OPTIONAL MATCH (b) --> (c: Resource), \
+                    (c) -[r_triangle]-> (n)  WHERE ANY (x IN c.skos__prefLabel WHERE x CONTAINS "@en")\
+                    RETURN b.uri AS node2, r AS relation, r2 AS relation2, COUNT(r_triangle) AS weight'
         result = self.session.run(query)
         return [self.map_related_nodes(record) for record in result]
 
