a71e204c4708a7d3bcef4f2bb0e609cd8aa41d7d
Patryk Stronski <p.stronski13@gmail.com>
2021-01-16 18:55:57 +0100

    changed labellify to wokr

--- tools/labellify.py
+++ tools/labellify.py
@@ -21,20 +21,30 @@ def transform_langtag(labels):
     ret = []
     for lab in labels:
        ret.append(PHRASE_SEPARATOR + lab[:-3] + PHRASE_SEPARATOR)
-    return " ".join(list(set(ret)))
+    return ret
 
 start = int(sys.argv[1])
 end = int(sys.argv[2])
 for node_ind in range(start, end):
     if node_ind % 1000 == 0:
         print(node_ind)
-    node = [{"elabels": rec["elabels"], "plabels": rec["plabels"], "uri": rec["uri"]} for rec in session.run('MATCH (n:Resource) RETURN n.uri AS uri, n.labels_polish AS plabels, n.labels_english AS elabels SKIP ' + str(node_ind) + ' LIMIT 1')][0]
-    if node["plabels"]:
-        lab_pl = PHRASE_SEPARATOR + PHRASE_SEPARATOR.join(node["plabels"].split(" ")) + PHRASE_SEPARATOR
+    node = [{"alabels": rec["alabels"], "plabels": rec["plabels"], "uri": rec["uri"]} for rec in session.run('MATCH (n:Resource) RETURN n.uri AS uri, n.skos__prefLabel AS plabels, n.skos_altLabel AS alabels SKIP ' + str(node_ind) + ' LIMIT 1')][0]
+    if not node["alabels"] and not node["plabels"]:
+        continue
+    if not node["alabels"]:
+        node["alabels"] = []
+    if not node["plabels"]:
+        node["plabels"] = []
+    labels = node["alabels"] + node["plabels"]
+    print(labels)
+    plabels = transform_langtag(list(filter(lambda l: l.endswith("@pl"),labels)))
+    elabels = transform_langtag(list(filter(lambda l: l.endswith("@en"),labels)))
+    if plabels:
+        lab_pl = PHRASE_SEPARATOR + PHRASE_SEPARATOR.join(plabels) + PHRASE_SEPARATOR
     else:
         lab_pl = ""
-    if node["elabels"]:
-        lab_en = PHRASE_SEPARATOR + PHRASE_SEPARATOR.join(node["elabels"].split(" ")) + PHRASE_SEPARATOR
+    if elabels:
+        lab_en = PHRASE_SEPARATOR + PHRASE_SEPARATOR.join(elabels) + PHRASE_SEPARATOR
     else:
         lab_en = ""
     session.run('MATCH (n: Resource {uri: "' + node["uri"] + '"}) SET n.labels_english = "'+ lab_en +'" SET n.labels_polish = "' + lab_pl + '"')
